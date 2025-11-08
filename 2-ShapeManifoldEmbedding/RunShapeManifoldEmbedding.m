d3ef6fef8818c98c6eee0cce81c85d445a0b5d89
samjefferyes <sdrjefferyes@gmail.com>
2025-11-08 14:36:36 +0000

    Adding dims variable

--- 2-ShapeManifoldEmbedding/RunShapeManifoldEmbedding.m
+++ 2-ShapeManifoldEmbedding/RunShapeManifoldEmbedding.m
@@ -6,7 +6,7 @@ global PATH;
 PATH=out.folder;
 if exist(dataFile, 'file')
     data = load(dataFile);
-    ShapeManifoldEmbedding_finalSJ(data.BigCellArray, out.folder, out.sparse);
+    ShapeManifoldEmbedding_finalSJ(data.BigCellArray, out.folder, out.sparse, out.dims);
 else 
     display('-------')
     display('The file "Bigcellarrayandindex.mat" does not exist in your Analysis folder.');
